e021eaf59c4dcc1bc59f4f55f784d154ac50eacc
Acoidán Rodríguez Peña <acoidanrp13@gmail.com>
2023-12-18 17:32:57 +0000

    Main y la aplicacion con SQLite esta acabada

--- src/main/java/software/ulpgc/kata4/Main.java
+++ src/main/java/software/ulpgc/kata4/Main.java
@@ -1,15 +1,21 @@
 package software.ulpgc.kata4;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 
 public class Main {
-    public static void main(String[] args) {
-        List<Person> people = TsvFilePersonLoader.with("hw_25000.tsv").load();
-        Histogram weightHistogram = new WeightHistogram(people);
-        MainFrame mainFrame = new MainFrame();
-        mainFrame.histogramDisplay().show(weightHistogram);
-        mainFrame.setVisible(true);
+    public static void main(String[] args) throws SQLException {
+        try(Connection connection = DriverManager.getConnection("jdbc:sqlite:db1.db")){
+            List<Person> people = new SQLitePersonLoader(connection).load();
+            Histogram weightHistogram = new WeightHistogram(people);
+            MainFrame mainFrame = new MainFrame();
+            mainFrame.histogramDisplay().show(weightHistogram);
+            mainFrame.setVisible(true);
+
+        }
     }
 }
 
